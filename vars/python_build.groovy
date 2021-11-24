61ecb163a909a8b45f34a0fd3e20e1bb60cbb781
Michael Jacinto <atmwhockey@hotmail.com>
2021-11-24 11:05:25 -0800

    chg dir

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -4,7 +4,7 @@ def call(dockerRepoName, imageName) {
         stages {
             stage('Build') {
                 steps {
-                    sh 'pip install -r azure-vote/azure-vote/requirements.txt'
+                    sh 'pip install -r requirements.txt'
                 }
             }
             stage('Python Lint') {
