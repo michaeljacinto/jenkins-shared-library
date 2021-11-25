5278f046906cbd899c59290b5efe77150c625327
Michael Jacinto <atmwhockey@hotmail.com>
2021-11-24 16:44:30 -0800

    add

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -25,9 +25,9 @@ def call(dockerRepoName, imageName) {
                 }
             }
             stage('Deploy') {
+                def ssh_cmd = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no azureuser@michaeljacinto.westus2.cloudapp.azure.com"
                 steps {
                     sshagent(credentials : ['kafka-key-pair']) {
-                        def ssh_cmd = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no azureuser@michaeljacinto.westus2.cloudapp.azure.com"
                         sh "${ssh_cmd} 'docker pull ${dockerRepoName}/${imageName}'"
                         sh "${ssh_cmd} 'docker-compose -f ./microservices/Deployment/docker-compose-4850.yml up -d'"
                     }
